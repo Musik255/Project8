f7db8bc123aaa4a4ddaa440498e66b4b1e6d2be0
P ch <pkchvyrov@vk.com>
2023-12-03 22:39:46 +0300

    completed task

--- Project8/Project8/SceneDelegate.swift
+++ Project8/Project8/SceneDelegate.swift
@@ -13,12 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        if let tabBarController = window?.rootViewController as? UITabBarController {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "NavController")
+            viewController.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+            tabBarController.viewControllers?.append(viewController)
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -47,6 +51,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
+    
 }
 
--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -10,35 +10,55 @@ import UIKit
 class TableViewController: UITableViewController {
     
     var petitions = [Petition]()
-    
+    var petitionsFiltered = [Petition]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
-
-        if let url = URL(string: urlString){
-            if let data = try? Data(contentsOf: url){
-                print("data success rdy to parse")
-                parse(json: data)
-            }
-                
+        let urlString : String
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(alertShowCredits))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(alertFilter))
+        
+        
+        
+        
+        
+        
+        if navigationController?.tabBarItem.tag == 0{
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         }
+        else{
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        
+        parse(urlString)
+        
+        filter(word: "")
+        
+        tableView.reloadData()
+    
+        
+        
        
     }
 
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return petitionsFiltered.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let petition = petitions[indexPath.row]
+        let petition = petitionsFiltered[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "IdentCell", for: indexPath)
         
         var content = cell.defaultContentConfiguration()
+        content.textProperties.numberOfLines = 1
+        content.secondaryTextProperties.numberOfLines = 1
         content.text = petition.title
         content.secondaryText = petition.body
         
@@ -47,14 +67,86 @@ class TableViewController: UITableViewController {
         return cell
     }
     
-    func parse(json: Data) {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let viewController = DetailViewController()
+        viewController.detailItem = petitionsFiltered[indexPath.row]
+        navigationController?.pushViewController(viewController, animated: true)
+        
+    }
+    
+    
+    func parse(_ urlString: String) {
+        
         let decoder = JSONDecoder()
+        
+        if let url = URL(string: urlString){
+            
+            if let data = try? Data(contentsOf: url){
 
-        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.results
-            tableView.reloadData()
+//                print("data success rdy to parse")
+
+                if let jsonPetitions = try? decoder.decode(Petitions.self, from: data) {
+                    petitions = jsonPetitions.results
+                    
+                }
+                return
+            }
         }
+        showErrParse()
+        
     }
 
+    
+    
+    func showErrParse(){
+        let alertController = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alertController, animated: true)
+    }
+    
+    
+    @objc func alertShowCredits(){
+        let action = UIAlertController(title: "Credits", message: "All data comes from the \"We The People API of the Whitehouse\".", preferredStyle: .alert)
+        action.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(action, animated: true)
+    }
+    
+    @objc func alertFilter(){
+        let alertController = UIAlertController(title: "Напишите слово, по которому будет производиться поиск", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Отправить", style: .default) { [weak self, weak alertController] action in
+            
+            guard let word = alertController?.textFields?[0].text else { return }
+//            self?.submit(answer)
+            self?.filter(word: word)
+            self?.tableView.reloadData()
+            
+            
+        }
+        alertController.addAction(submitAction)
+        present(alertController, animated: true)
+    }
+    
+    
+    
+    
+    
+    func filter(word : String){
+        petitionsFiltered = [Petition]()
+        if word.isEmpty{
+            petitionsFiltered = petitions
+        }
+        else{
+            for petition in petitions {
+                
+                if petition.body.lowercased().contains(word.lowercased()) || petition.title.lowercased().contains(word.lowercased()) {
+                    petitionsFiltered.append(petition)
+                }
+            }
+        }
+    }
+    
 }
 
